f89830394566a3e99aad611dda995b460f5b9fd8
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2024-03-25 15:03:23 +0500

    work

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use('/auth',UserRoute)
-app.use('/test' , requireSignIn, testController)
+app.use('/test', requireSignIn, testController)
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'Successfully'
+  })
+})
 const PORT = 8000 
+
 
 mongoose.connect("mongodb+srv://asmamughal097:UZDynR7zE9b5qTYm@cluster0.jlpafag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => app.listen(PORT, ()=> console.log("Server runing")))
     .catch(err => console.error("MongoDB connection error:", err));
-    app.get('*',(req,res,next)=>{
-        res.status(200).json({
-          message:'Successfully'
-        })
-      })
+    
