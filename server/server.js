80f51e337ba7b76d9ce3d9654bbc2be136609324
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2024-03-25 14:47:22 +0500

    wor

--- server/server.js
+++ server/server.js
@@ -16,3 +16,8 @@ const PORT = 8000
 mongoose.connect("mongodb+srv://asmamughal097:UZDynR7zE9b5qTYm@cluster0.jlpafag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => app.listen(PORT, ()=> console.log("Server runing")))
     .catch(err => console.error("MongoDB connection error:", err));
+    app.get('*',(req,res,next)=>{
+        res.status(200).json({
+          message:'Successfully'
+        })
+      })
